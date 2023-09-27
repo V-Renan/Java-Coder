a7af58271400c492923cc54e90f3f3e5f3dfa6aa
V-Renan <victorrenan2018@gmail.com>
2023-09-27 03:40:15 -0300

    controle
    for3

---
+++ Java/exercicios/src/controle/For3.java
@@ -0,0 +1,24 @@
+package controle;
+
+/**
+ * @author Victor$
+ * @date 9/27/2023$
+ * Description:
+ */
+public class For3 {
+
+    public static void main(String[] args) {
+
+
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                System.out.printf("[%d %d]", i, j);
+            }
+            System.out.println("");
+        }
+
+        int i = 0;
+        System.out.println("Fim do FOR...");
+        System.out.println(i);
+    }
+}
