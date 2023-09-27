c87164f4e5e7fa86c1490f5e5eabc9912a5ba1cf
V-Renan <victorrenan2018@gmail.com>
2023-09-27 02:17:01 -0300

    controle
    for--

---
+++ Java/exercicios/src/controle/For2.java
@@ -0,0 +1,16 @@
+package controle;
+
+/**
+ * @author Victor$
+ * @date 9/27/2023$
+ * Description:
+ */
+public class For2 {
+
+    public static void main(String[] args) {
+
+        for (int contador=10; contador>=0; contador -=2) {
+            System.out.println("Valor do contador: " + contador);
+        }
+    }
+}
