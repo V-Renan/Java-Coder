1a9fce480f182dce203b7fd0cb66f4542494ea32
V-Renan <victorrenan2018@gmail.com>
2023-09-28 13:14:57 -0300

    exemplo
    up

---
+++ Java/exercicios/src/classe/Exemplo.java
@@ -0,0 +1,19 @@
+package classe;
+
+/**
+ * @author Victor$
+ * @date 9/28/2023$
+ * Description:
+ */
+public class Exemplo {
+
+    public static void main(String[] args) {
+        somar(1,2);
+    }
+
+    private static int somar(int a, int b) {
+        int resultado = a + b;
+        return resultado;
+    }
+
+}
