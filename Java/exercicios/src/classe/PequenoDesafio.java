649248f3ebf74d362a0d06c03cba47b923e16239
V-Renan <victorrenan2018@gmail.com>
2023-09-29 00:33:27 -0300

    pequenoDesafio
    um pequeno desafio de instacia

---
+++ Java/exercicios/src/classe/PequenoDesafio.java
@@ -0,0 +1,20 @@
+package classe;
+
+/**
+ * @author Victor$
+ * @date 9/29/2023$
+ * Description:
+ */
+public class PequenoDesafio {
+
+    int a = 3;
+    static int b = 4;
+
+    public static void main(String[] args) {
+
+        PequenoDesafio teste = new PequenoDesafio();
+        System.out.println(teste.a);
+        System.out.println(b);
+
+    }
+}
