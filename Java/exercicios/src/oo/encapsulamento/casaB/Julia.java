defc9daef07bff5a4dcaf12a9dac56ae5a39c2f4
V-Renan <victorrenan2018@gmail.com>
2023-10-18 12:49:45 -0300

    modificadores de acesso
    oo

--- Java/exercicios/src/oo/encapsulamento/casaB/Julia.java
+++ Java/exercicios/src/oo/encapsulamento/casaB/Julia.java
@@ -1,9 +1,19 @@
 package oo.encapsulamento.casaB;
 
+import oo.encapsulamento.casaA.Ana;
+
 /**
  * @author Victor$
  * @date 10/18/2023$
  * Description:
  */
 public class Julia {
+
+    Ana sogra = new Ana();
+    void testeAcessos() {
+        //System.out.println(sogra.segredo);
+        //System.out.println(sogra.facoDentroDeCasa);
+        //System.out.println(sogra.formaDeFalar);
+        System.out.println(sogra.todosSabem);
+    }
 }
--- Java/exercicios/src/oo/encapsulamento/casaB/Pedro.java
+++ Java/exercicios/src/oo/encapsulamento/casaB/Pedro.java
@@ -11,9 +11,9 @@ public class Pedro extends Ana {
 
     Ana mae = new Ana();
     void testeAcessos() {
-        //System.out.println(esposa.segredo);
+        //System.out.println(mae.segredo);
         //System.out.println(mae.facoDentroDeCasa);
-        //System.out.println(mae.formaDeFalar);
-        System.out.println(mae.todosSabem);
+        System.out.println(formaDeFalar);
+        System.out.println(todosSabem);
     }
 }
