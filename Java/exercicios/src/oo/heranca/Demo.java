f7a1f97ad5e5c7124ed3c42e2350a1db7b7cd177
V-Renan <victorrenan2018@gmail.com>
2023-10-18 11:14:35 -0300

    desafio-oo

--- Java/exercicios/src/oo/heranca/Demo.java
+++ Java/exercicios/src/oo/heranca/Demo.java
@@ -13,9 +13,9 @@ public class Demo {
         monstro.x = 10;
         monstro.y = 10;
 
-        Heroi heroi = new Heroi();
-        heroi.x = 10;
-        heroi.y = 11;
+        Heroi heroi = new Heroi(10, 11);
+//        heroi.x = 10;
+//        heroi.y = 11;
 
         System.out.println("Monstro tem --> " + monstro.vida);
         System.out.println("Heroi tem --> " + heroi.vida);
--- Java/exercicios/src/oo/heranca/Monstro.java
+++ Java/exercicios/src/oo/heranca/Monstro.java
@@ -6,4 +6,12 @@ package oo.heranca;
  * Description:
  */
 public class Monstro extends Jogador{
+
+    Monstro() {
+        this(0, 0);
+    }
+
+    Monstro(int x, int y) {
+        super(x, y);
+    }
 }
