a0a764befdd95060f015573503abe83ce5c37a4f
V-Renan <victorrenan2018@gmail.com>
2023-11-14 06:45:00 -0300

    campo-minado
    definindo tabuleiro

--- Java/campo-minado/src/br/com/victor/cm/modelo/Tabuleiro.java
+++ Java/campo-minado/src/br/com/victor/cm/modelo/Tabuleiro.java
@@ -1,9 +1,48 @@
 package br.com.victor.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Victor$
  * @date 11/7/2023$
  * Description:
  */
 public class Tabuleiro {
+
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    private final List<Campo> campos = new ArrayList<>();
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarVizinhos();
+        sortearMinas();
+    }
+
+    private void gerarCampos() {
+        for (int l = 0; l < linhas; l++) {
+            for (int c = 0; c < colunas; c++) {
+                campos.add(new Campo(l, c));
+            }
+        }
+    }
+
+    private void associarVizinhos() {
+        for (Campo c1 : campos) {
+            for (Campo c2 : campos) {
+                c1.adicionarVizinho(c2);
+            }
+        }
+    }
+    
+    private void sortearMinas() {
+        
+    }
 }
