ff5d81fd49cd3784a37a543b1b82043b1dfff1a8
V-Renan <victorrenan2018@gmail.com>
2023-12-14 13:09:05 -0300

    DAO
    adicionando produto

---
+++ Java/exercicios-jpa/src/infra/DAO.java
@@ -0,0 +1,71 @@
+package infra;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * @author Victor$
+ * @date 14/12/2023$
+ * Description:
+ */
+public class DAO<E> {
+
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+    private Class<E> classe;
+
+    static {
+        try {
+            emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+        } catch (Exception e) {
+            // logar -> log4j
+        }
+    }
+
+    public DAO() {
+        this(null);
+    }
+
+    public DAO(Class<E> classe) {
+        this.classe = classe;
+        em = emf.createEntityManager();
+    }
+
+    public DAO<E> abrirTransacao() {
+        em.getTransaction().begin();
+        return this;
+    }
+
+    public DAO<E> fecharTransacao() {
+        em.getTransaction().commit();
+        return this;
+    }
+
+    public DAO<E> incluir(E entidade) {
+        em.persist(entidade);
+        return this;
+    }
+
+    public DAO<E> incluirAtomico(E entidade) {
+        return this.abrirTransacao().incluir(entidade).fecharTransacao();
+    }
+
+    public List<E> obterTodos() {
+        return this.obterTodos(10,0);
+    }
+    public List<E> obterTodos(int qtde, int deslocamento) {
+        if (classe == null) {
+            throw new UnsupportedOperationException("Classe nula.");
+        }
+
+        String jpql = "SELECT e FROM " + classe.getName() + " e";
+        TypedQuery<E> query = em.createQuery(jpql, classe);
+        query.setMaxResults(qtde);
+        query.setFirstResult(deslocamento);
+        return query.getResultList();
+    }
+
+    public void fechar() {
+        em.close();
+    }
+}
---
+++ Java/exercicios-jpa/src/teste/basico/NovoProduto.java
@@ -0,0 +1,22 @@
+package teste.basico;
+
+import infra.DAO;
+import modelo.basico.Produto;
+
+/**
+ * @author Victor$
+ * @date 14/12/2023$
+ * Description:
+ */
+public class NovoProduto {
+
+    public static void main(String[] args) {
+
+        DAO<Produto> dao = new DAO<>(Produto.class);
+
+        Produto produto = new Produto("Monitor 23″", 789.98);
+        dao.incluirAtomico(produto).fechar();
+
+        System.out.println("Id do produto: " + produto.getId());
+    }
+}
