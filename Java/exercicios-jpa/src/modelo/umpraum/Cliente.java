43b49d8657876f9ed6dbbd596db58ef0911edcae
V-Renan <victorrenan2018@gmail.com>
2024-01-04 10:30:22 -0300

    jpa
    cascade

--- Java/exercicios-jpa/src/modelo/umpraum/Cliente.java
+++ Java/exercicios-jpa/src/modelo/umpraum/Cliente.java
@@ -15,7 +15,7 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "assento_id", unique = true)
     private Assento assento;
 
---
+++ Java/exercicios-jpa/src/teste/basico/umpraum/NovoClienteAssento2.java
@@ -0,0 +1,23 @@
+package teste.basico.umpraum;
+
+import infra.DAO;
+import modelo.umpraum.Assento;
+import modelo.umpraum.Cliente;
+
+/**
+ * @author Victor$
+ * @date 04/01/2024$
+ * Description:
+ */
+public class NovoClienteAssento2 {
+
+    public static void main(String[] args) {
+
+        Assento assento = new Assento("4D");
+        Cliente cliente = new Cliente("Maria", assento);
+
+        DAO<Cliente> dao = new DAO<>();
+
+        dao.incluirAtomico(cliente);
+    }
+}
