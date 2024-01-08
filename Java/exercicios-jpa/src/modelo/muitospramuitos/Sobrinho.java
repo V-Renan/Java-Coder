7030602df535db0c9b75e71e0df646d241a5672d
V-Renan <victorrenan2018@gmail.com>
2024-01-08 08:01:20 -0300

    jpa
    muitosPraMuitos

---
+++ Java/exercicios-jpa/src/modelo/muitospramuitos/Sobrinho.java
@@ -0,0 +1,53 @@
+package modelo.muitospramuitos;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Victor$
+ * @date 08/01/2024$
+ * Description:
+ */
+
+@Entity
+public class Sobrinho {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @ManyToMany(mappedBy = "sobrinhos")
+    private List<Tio> tios = new ArrayList<>();
+
+    public Sobrinho() {
+
+    }
+
+    public Sobrinho(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Tio> getTios() {
+        return tios;
+    }
+
+    public void setTios(List<Tio> tios) {
+        this.tios = tios;
+    }
+}
