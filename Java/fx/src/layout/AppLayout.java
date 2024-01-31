ca871605ae779cca3cdb66f03b263755b391316c
V-Renan <victorrenan2018@gmail.com>
2024-01-30 21:36:39 -0300

    javaFX
    LayOut

--- Java/fx/src/layout/AppLayout.java
+++ Java/fx/src/layout/AppLayout.java
@@ -3,7 +3,6 @@ package layout;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -15,27 +14,13 @@ public class AppLayout extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent raiz = null;
-//        VBox temp = new VBox();
-//        temp.getChildren().add(new Caixa().comTexto("1"));
-//        temp.getChildren().add(new Caixa().comTexto("2"));
-//        temp.getChildren().add(new Caixa().comTexto("3"));
-//        temp.getChildren().add(new Caixa().comTexto("4"));
-//        temp.getChildren().add(new Caixa().comTexto("5"));
-//        temp.getChildren().add(new Caixa().comTexto("6"));
-//        temp.getChildren().add(new Caixa().comTexto("7"));
 
-//        temp.getChildren().add(new Quadrado());
-//        temp.getChildren().add(new Quadrado());
-//        temp.getChildren().add(new Quadrado());
-//        temp.getChildren().add(new Quadrado());
-//        temp.getChildren().add(new Quadrado());
-//        temp.getChildren().add(new Quadrado());
-//        temp.getChildren().add(new Quadrado());
-        raiz = new TesteAnchorPane();
-        raiz = new TesteBorderPane();
-        raiz = new TesteFlowPane();
-        raiz = new TesteGridPane();
-        raiz = new TesteStackPane();
+//        raiz = new TesteAnchorPane();
+//        raiz = new TesteBorderPane();
+//        raiz = new TesteFlowPane();
+//        raiz = new TesteGridPane();
+//        raiz = new TesteStackPane();
+        raiz = new TesteTilePane();
 
         Scene principal = new Scene(raiz,800,600);
         stage.setScene(principal);
