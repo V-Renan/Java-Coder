53be7d6ed5030e88bf9d774eb8e1454908253a0d
V-Renan <victorrenan2018@gmail.com>
2024-02-06 04:21:30 -0300

    springBoot
    cntroller

--- Java/exercicios-sb/src/main/java/br/com/victor/exerciciossb/controllers/ClienteController.java
+++ Java/exercicios-sb/src/main/java/br/com/victor/exerciciossb/controllers/ClienteController.java
@@ -1,9 +1,7 @@
 package br.com.victor.exerciciossb.controllers;
 
 import br.com.victor.exerciciossb.models.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Victor$
@@ -16,5 +14,13 @@ public class ClienteController {
     @GetMapping(path = "/qualquer")
     public Cliente obterCliente() {
         return new Cliente(28,"Pedro", "123,456,789-00");
+    }
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id) {
+        return new Cliente(id, "Maria", "987.654.321-00");
+    }
+    @GetMapping
+    public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+        return new Cliente(id, "Victor", "111.222.333-44");
     }
 }
